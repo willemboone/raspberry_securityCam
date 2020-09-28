187bc97da9bd9b33aa2cea1b8487d80a5ed94868
willem boone <willem.boon@outlook.com>
2020-09-28 11:35:11 +0200

    update

--- google_drive.py
+++ google_drive.py
@@ -12,7 +12,7 @@ class G_Drive(object):
         gauth.LocalWebserverAuth()
         self.drive = GoogleDrive(gauth)
     
-    def upload(self, file, fid = "1oZQ20r_IW7kbgCF8OzJN671scfw8oJ8w"):
+    def upload(self, file, fid):
         f = self.drive.CreateFile({"parents": [{"kind": "drive#fileLink", "id": fid}]})
         f.SetContentFile(file)  # Read local file
         f.Upload()
